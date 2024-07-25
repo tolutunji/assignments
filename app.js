f1cb88b7e960c9ee1b2f4b97e81a500b140f1709
olubobokun tolulope <olubobokuntolulope@gmail.com>
2024-07-25 17:38:50 +0100

    complete parsing http methods

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ const port = 5500;
 const server = http.createServer((req, res) => {
 
     const parsedUrl = url.parse(req.url, true);
-    console.log(parsedUrl)
+    console.log(parsedUrl);
+
+    const path = parsedUrl.pathname;
+    console.log(path);
+
+    const trimmedPath =  path.replace(/^\/+|\/+$/g, '');
+    console.log(trimmedPath);
+
+    const method = req.method.toLowerCase();
+    console.log(method);
 
 });
 
--- index.js
+++ index.js
@@ -12,7 +12,16 @@ const port = 5700;
 const server = http.createServer(function(req, res) {
 
     const parsedUrl = url.parse(req.url, true);
-    console.log(parsedUrl)
+    console.log(parsedUrl);
+
+    const path = parsedUrl.pathname;
+    console.log(path);
+
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    console.log(trimmedPath);
+
+    const method = req.method.toLowerCase();
+    console.log(method);
 
 });
 
